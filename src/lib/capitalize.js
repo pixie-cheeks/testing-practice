49afdccc0375eb3c0d7ba9566aca542fc48eb240
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2024-06-02 20:52:15 +0530

    Finish working on capitalize

--- src/lib/capitalize.js
+++ src/lib/capitalize.js
@@ -1,3 +1,3 @@
-const capitalize = () => 'Name';
+const capitalize = (string) => string.charAt(0).toUpperCase() + string.slice(1);
 
 export default capitalize;
--- src/tests/capitalize.test.js
+++ src/tests/capitalize.test.js
@@ -1,5 +1,16 @@
 import capitalize from '../lib/capitalize.js';
 
-test('Takes a string and returns it capitalized', () => {
-  expect(capitalize('name')).toBe('Name');
+describe('capitalize function', () => {
+  test('Takes a string & returns it with the first letter capitalized', () => {
+    expect(capitalize('name')).toBe('Name');
+  });
+
+  test('Works with any input string', () => {
+    expect(capitalize('golem')).toBe('Golem');
+    expect(capitalize('my name is pikachu')).toBe('My name is pikachu');
+  });
+
+  test('Works on empty string', () => {
+    expect(capitalize('')).toBe('');
+  });
 });
