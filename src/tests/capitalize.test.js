b024f7347c379184a870a6df04384f054ad74085
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2024-06-03 03:07:06 +0530

    Remove unnecessary describe function

--- src/tests/capitalize.test.js
+++ src/tests/capitalize.test.js
@@ -1,16 +1,14 @@
 import capitalize from '../lib/capitalize.js';
 
-describe('capitalize function', () => {
-  test('Takes a string & returns it with the first letter capitalized', () => {
-    expect(capitalize('name')).toBe('Name');
-  });
+test('Takes a string & returns it with the first letter capitalized', () => {
+  expect(capitalize('name')).toBe('Name');
+});
 
-  test('Works with any input string', () => {
-    expect(capitalize('golem')).toBe('Golem');
-    expect(capitalize('my name is pikachu')).toBe('My name is pikachu');
-  });
+test('Works with any input string', () => {
+  expect(capitalize('golem')).toBe('Golem');
+  expect(capitalize('my name is pikachu')).toBe('My name is pikachu');
+});
 
-  test('Works on empty string', () => {
-    expect(capitalize('')).toBe('');
-  });
+test('Works on empty string', () => {
+  expect(capitalize('')).toBe('');
 });
--- src/tests/reverse-string.test.js
+++ src/tests/reverse-string.test.js
@@ -1,15 +1,13 @@
 import reverseString from '../lib/reverse-string.js';
 
-describe('reverseString function', () => {
-  test('Takes a string and returns it reversed', () => {
-    expect(reverseString('wonder')).toBe('rednow');
-  });
+test('Takes a string and returns it reversed', () => {
+  expect(reverseString('wonder')).toBe('rednow');
+});
 
-  test('Works on another string', () => {
-    expect(reverseString('hello')).toBe('olleh');
-  });
+test('Works on another string', () => {
+  expect(reverseString('hello')).toBe('olleh');
+});
 
-  test('Works on any string', () => {
-    expect(reverseString('blah')).toBe('halb');
-  });
+test('Works on any string', () => {
+  expect(reverseString('blah')).toBe('halb');
 });
