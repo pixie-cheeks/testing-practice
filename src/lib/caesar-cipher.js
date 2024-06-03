a30daa2d456a30cf4d8631e93e7e816415a12f09
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2024-06-03 16:55:31 +0530

    Finish working on caesarCipher

--- src/lib/caesar-cipher.js
+++ src/lib/caesar-cipher.js
@@ -1,3 +1,27 @@
-const caesarCipher = () => 'def';
+const isUpperCase = (charCode) => charCode >= 65 && charCode <= 90;
+const isLowerCase = (charCode) => charCode >= 97 && charCode <= 122;
+
+const generateLetter = (charCode, startCode, endCode) =>
+  charCode <= endCode
+    ? String.fromCharCode(charCode)
+    : String.fromCharCode(startCode - 1 + (charCode % endCode));
+
+const mapCharacter = (character, shiftFactor) => {
+  const charCode = character.charCodeAt(0);
+
+  if (isUpperCase(charCode))
+    return generateLetter(charCode + shiftFactor, 65, 90);
+
+  if (isLowerCase(charCode))
+    return generateLetter(charCode + shiftFactor, 97, 122);
+
+  return character;
+};
+
+const caesarCipher = (string, shiftFactor) =>
+  string
+    .split('')
+    .map((character) => mapCharacter(character, shiftFactor))
+    .join('');
 
 export default caesarCipher;
--- src/test/caesar-cipher.test.js
+++ src/test/caesar-cipher.test.js
@@ -3,3 +3,34 @@ import caesarCipher from '../lib/caesar-cipher.js';
 test('Shifts each letter in a string by the shift factor', () => {
   expect(caesarCipher('abc', 3)).toBe('def');
 });
+
+test('Works on more than one input', () => {
+  expect(caesarCipher('abc', 1)).toBe('bcd');
+  expect(caesarCipher('abc', 2)).toBe('cde');
+  expect(caesarCipher('def', 2)).toBe('fgh');
+  expect(caesarCipher('def', 1)).toBe('efg');
+});
+
+test('Wraps from z to a', () => {
+  expect(caesarCipher('xyz', 3)).toBe('abc');
+  expect(caesarCipher('xyz', 4)).toBe('bcd');
+  expect(caesarCipher('xyz', 1)).toBe('yza');
+  expect(caesarCipher('bcd', 26)).toBe('bcd');
+});
+
+test('Preserves case', () => {
+  expect(caesarCipher('HeLLo', 3)).toBe('KhOOr');
+  expect(caesarCipher('ZigZag', 1)).toBe('AjhAbh');
+  expect(caesarCipher('ABCD', 26)).toBe('ABCD');
+  expect(caesarCipher('Hello Is', 26)).toBe('Hello Is');
+});
+
+test('Preserves Punctuation', () => {
+  expect(caesarCipher('Hello, World!', 3)).toBe('Khoor, Zruog!');
+  expect(
+    caesarCipher('Hello there! Is this the right time for this?', 26),
+  ).toBe('Hello there! Is this the right time for this?');
+  expect(caesarCipher('1, 2, 3, 4, 5 and then 6', 26)).toBe(
+    '1, 2, 3, 4, 5 and then 6',
+  );
+});
